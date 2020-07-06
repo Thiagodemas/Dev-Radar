2857e46b1b7a798171d70e5fdc77ec053cfe2e60
Thiago Demas <thiago.demas7@gmail.com>
2020-07-06 15:04:07 -0300

    Update components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import api from './services/api';
-
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-
 function App() {
   const [devs, setDevs] = useState([]);
-
-
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const {latitude, longitude} = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err)
-      },
-      {
-        timeout: 30000,
-      }
-    )
-  }, []);
 
   useEffect(() => {
     async function loadDevs() {
@@ -39,23 +17,11 @@ function App() {
 
       setDevs(response.data);
     }
-
     loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
-
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude,
-    })
-
-    setGithubUsername('');
-    setTechs('');
-
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
     setDevs([...devs, response.data]);
   }
 
@@ -63,77 +29,12 @@ function App() {
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              required
-              id="github_username"
-              type="text"
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              type="text"
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                required
-                id="latitude"
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                required
-                id="longitude"
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
           { devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img
-                  src={dev.avatar_url}
-                  alt={dev.name}/>
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-              <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
-            </li>
+            <DevItem key={dev._id} dev={dev}/>
           ))}
         </ul>
       </main>
